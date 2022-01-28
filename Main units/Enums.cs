c6da85c23563f6ea97628bc5cd52109af49a036a
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 09:34:56 +0300

    add enums

---
+++ Main units/Enums.cs
@@ -0,0 +1,29 @@
+﻿namespace ChessLibrary.Main_units
+{
+    /// <summary>
+    /// Side enum represents white or black side
+    /// </summary>
+    public enum SideEnum
+    {
+        White,
+        Black
+    }
+    /// <summary>
+    /// Statement of figure. Needs for pawns (if theq DONTMOVED then they can move at 2 fields forvard), 
+    /// for pawns takeing on the pass and for king castling.
+    /// </summary>
+    public enum FigureStateEnum
+    {
+        NONE,
+        DONTMOVED,
+        MOVED
+    }
+    /// <summary>
+    /// Only for pawns. Needs for calculations Y-movements of pawns.
+    /// </summary>
+    public enum DirectionOfMovementEnum
+    {
+        UP = 1,
+        DOWN = -1,
+    }
+}
