c145145dda80c96e7024fc997a53cdfaa707a3a9
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 11:05:04 +0300

    add abstract figures factory

--- Factories/FiguresFactory.cs
+++ Factories/FiguresFactory.cs
@@ -1,8 +1,25 @@
-﻿
+﻿using ChessLibrary.Main_units;
+
 namespace ChessLibrary.Factories
 {
+    /// <summary>
+    /// Creating all types of figures.
+    /// </summary>
     public abstract class FiguresFactory
     {
+        protected SideEnum factorySide;
+        protected DirectionOfMovementEnum pawnDirection;
+        public abstract Figure CreateKing();
+        public abstract Figure CreateQueen();
+        public abstract Figure CreateRook();
+        public abstract Figure CreateBishop();
+        public abstract Figure CreateKnight();
+        public abstract Figure CreatePawn();
 
+        public void SetFactorySide(SideEnum side) => factorySide = side;
+        /// <summary>
+        /// Pawn movement direction. It depends by side.
+        /// </summary>
+        public void SetPawnMovementDirection(DirectionOfMovementEnum direction) => pawnDirection = direction;
     }
 }
