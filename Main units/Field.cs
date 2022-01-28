01cecf49e35a937bb3e1bc4b0d08c4358f613de2
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 10:02:15 +0300

    add field

--- Main units/Field.cs
+++ Main units/Field.cs
@@ -1,8 +1,33 @@
 ﻿
 namespace ChessLibrary.Main_units
 {
+    /// <summary>
+    /// Class that represents a singe field in board[Y,X]
+    /// That may containts figure 
+    /// </summary>
     public class Field
     {
+        public Field(int y, int x)
+        {
+            X = x;
+            Y = y;
+        }
 
+        public Figure? Figure { get; set; }
+        public bool IsEmpty => Figure == null;
+
+
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public override string ToString() => $"Field at y = {Y}, x = {X}, contains: {Figure?.ToString() ?? "nothing"}";
+
+        public static Field FindFieldByFigure(Field[,] fields, Figure figure)
+        {
+            foreach (var field in fields)
+                if (field.Figure == figure) return field;
+            throw new Exception("");
+        }
     }
 }
