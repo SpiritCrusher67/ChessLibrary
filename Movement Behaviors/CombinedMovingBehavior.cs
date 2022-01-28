69a36ef60b05a8d7e5366784e3cfe8695e95699e
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 10:51:38 +0300

    add combined movements

---
+++ Movement Behaviors/CombinedMovingBehavior.cs
@@ -0,0 +1,27 @@
+﻿using ChessLibrary.Main_units;
+
+namespace ChessLibrary.Movement_Behaviors
+{
+    /// <summary>
+    /// It combine two behaviors. Properties throws NotImplementedException.
+    /// </summary>
+    public class CombinedMovingBehavior : IMovementBehavior
+    {
+        IMovementBehavior firstMovingBehavior;
+        IMovementBehavior secondMovingBehavior;
+
+        public CombinedMovingBehavior(IMovementBehavior firstBehavior, IMovementBehavior secondBehavior)
+        {
+            firstMovingBehavior = firstBehavior;
+            secondMovingBehavior = secondBehavior;
+        }
+
+        bool IMovementBehavior.IsSingleMoving => throw new NotImplementedException();
+
+        int IMovementBehavior.BoardDimension => throw new NotImplementedException();
+
+        public IEnumerable<Field> GetAvaliableMoves(Field[,] fields, (int y, int x) currentPosition, SideEnum currentSide) =>
+            firstMovingBehavior.GetAvaliableMoves(fields, currentPosition, currentSide)
+            .Concat(secondMovingBehavior.GetAvaliableMoves(fields, currentPosition, currentSide));
+    }
+}
