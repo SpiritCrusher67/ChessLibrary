70d5777f6e4fad0a2176741d927e5537f0309990
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 09:48:29 +0300

    takeing behavior interface updated

--- Takeing Behaviors/ITakeingBehavior.cs
+++ Takeing Behaviors/ITakeingBehavior.cs
@@ -1,8 +1,15 @@
-﻿
+﻿using ChessLibrary.Main_units;
+
 namespace ChessLibrary.Takeing_Behaviors
 {
+    /// <summary>
+    /// Needs for takeing figure by other figure. It can take figure and contain taked figures.
+    /// </summary>
     public interface ITakeingBehavior
     {
+        public void TakeFigureFromField(Field field);
+        public IEnumerable<Figure> GetTakedFigures { get; }
 
+        public event Action<Figure> FigureHasTakedEvent;
     }
 }
